e35137aa09887d1163c927b1e06bc34ae0d9aa52
Tim <timzhang3@gmail.com>
2020-06-07 23:06:23 +1000

    Started tut02

---
+++ q1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+typedef struct _node {
+   int data;
+   struct _node *next;
+} Node;
+typedef Node *List;
+
+int length(List L) {
+    
+}
+
---
+++ q4.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+
+// x^n
+// Iterative mindset: x * x * x * x * x ...  * x
+int pow(int x, unsigned int n) {
+   int result = 1;
+    for (int i = 0; i < n; i++) {
+        result *= x;
+    }
+   return result;
+}
+
+// x^n
+// Recursive mindset: x * x^(n-1)
+// x^(n-1)
+// x^(n-1) = x * x^(n-2)
+// x^(n-2)
+// x^(n-2) = x * x^(n-3)
+// ...
+// 1 = x
+
+int pow2(int x, unsigned int n) {
+    if (n == 0) {
+        return 1;
+    }
+    return x * pow2(x, n - 1); 
+}
+
