7d6e9a37e1b4b6e898e4403c050e86bbdb376113
Tim <timzhang3@gmail.com>
2020-08-07 14:56:23 +1000

    Resetted num-reachable

--- practice-material/Graphs/numReachable/numReachable.c
+++ practice-material/Graphs/numReachable/numReachable.c
@@ -6,17 +6,6 @@
 #include "Queue.h"
 #include "Graph.h"
 
-int getNumReachable(Graph g, int src, bool *visited) {
-	visted[src] = true;
-	int total = 0;
-	for (Vertex v = 0; v < GraphNumVertices(g); v++) {
-		if (GraphIsAdjacent(g, src, v) && !visited[src]) {
-			total +=  
-		}
-	}
-	return total;
-}
-
 int numReachable(Graph g, int src) {
 
 }
