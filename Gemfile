4ab1d2850302794b015a806cf58dece38e4ca96c
Tim Zhang <timzhang3@gmail.com>
2020-09-28 12:34:11 +0000

    Deployed working ruby menu + colourisation

--- Gemfile
+++ Gemfile
@@ -1,2 +1,4 @@
 source 'https://rubygems.org'
 gem "tty-prompt"
+gem "colorize"
+
--- terminal-menu.rb
+++ terminal-menu.rb
@@ -1,12 +1,13 @@
+# Startup: gotty -w --title-format "Data Structures & Algorithms" ruby terminal 
 require "tty-prompt"   
-
+require "colorize"
 
 prompt = TTY::Prompt.new
 
 begin
-    choices = ["Linked List", "Binary Search Tree", "AVL Tree", "Splay Tree"]
-    selectedIndex = prompt.select("Select a data structure", choices)
-    puts "Starting: %s\n" % [selectedIndex]
+    choices = ["Linked List", "Binary Search Tree", "AVL Tree", "Splay Tree", "Get me out of here"]
+    selectedIndex = prompt.select("Select a data structure".yellow, choices)
+    puts "Starting: %s\n".blue % [selectedIndex]
     case selectedIndex
     when "Linked List"
         system("linked-list/iterative-version/testLinkedList")
@@ -16,6 +17,8 @@ begin
         system("avl-tree/testTree")
     when "Splay Tree"
         system("splay-tree/testTree")
+    when "Get me out of here"
+        puts "Bye ;("
     end
 rescue SignalException => e
     nil
