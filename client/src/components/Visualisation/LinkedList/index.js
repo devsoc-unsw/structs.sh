6234bfe86342114a61cfdc4736ee0ba02fab7f34
Tymotex <timzhang3@gmail.com>
2021-06-19 17:42:04 +1000

    Converted Dom's visualiser into React component

--- client/src/components/Visualisation/LinkedList/index.js
+++ client/src/components/Visualisation/LinkedList/index.js
@@ -1,2 +1,2 @@
-export { default as LinkedList } from './LinkedListJoanna.jsx';
-// export { default as LinkedList } from './LinkedListDom.jsx';
+// export { default as LinkedList } from './LinkedListJoanna.jsx';
+export { default as LinkedList } from './LinkedListDom.jsx';
