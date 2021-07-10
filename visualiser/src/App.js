97459eb03a00ebf5123fb506a1651d2bb353c7d4
Sam Shi <yunzeshi@outlook.com>
2021-07-11 00:16:33 +1000

    Extracted all DOM api to App.js

--- visualiser/src/App.js
+++ visualiser/src/App.js
@@ -1,26 +1,47 @@
-import React, { useEffect } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import LinkedListAnimation from './Animation/LinkedList/linkedListAnimation';
 
 function App() {
+
+  // Setting up the list animation instance with default values
+  const defaultListValues = [2, 3, 4, 5];
+  const [list, setList] = useState();
+  const nodesGroupRef = useRef();
+  const pointersGroupRef = useRef();
   useEffect(() => {
-    const list = new LinkedListAnimation();
+    const newList = new LinkedListAnimation(defaultListValues ,nodesGroupRef.current, pointersGroupRef.current)
+    setList(newList);
   }, []);
+
+  // Managing user input from the forms
+  const [valueAppended, setValueAppended] = useState("");
+  const [indexDeleted, setIndexDeleted] = useState("");
+
+  const handleAppend = (e) => {
+    e.preventDefault();
+    list && list.animateAppend(parseInt(valueAppended));
+  }
+
+  const handleDelete = (e) => {
+    e.preventDefault();
+    list && list.animateDelete(parseInt(indexDeleted));
+  }
 
   return (
     <div classname="App">
       <header classname="App-header">
-        <form className="list-append-form">
-          <input name="value" placeholder="number to append" />
+        <form className="list-append-form" onSubmit={handleAppend}>
+          <input name="value" placeholder="number to append" value={valueAppended} onChange={e => setValueAppended(e.target.value)}/>
           <button type="submit" name="append">append</button>
         </form>
-        <form className="list-delete-form">
-          <input name="index" placeholder="position to delete" />
+        <form className="list-delete-form" onSubmit={handleDelete}>
+          <input name="index" placeholder="position to delete" value={indexDeleted} onChange={e => setIndexDeleted(e.target.value)} />
           <button type="submit" name="delete">delete </button>
         </form>
         <div className="visualiser">
           <svg className="visualiser-svg" overflow="auto" style={{ width: '100%' }}>
-            <g className="nodes" transform="translate(0, 20)" />
-            <g className="pointers" transform="translate(0, 20)" />
+            <g ref={nodesGroupRef} className="nodes" transform="translate(0, 20)"/>
+            <g ref={pointersGroupRef} className="pointers" transform="translate(0, 20)" />
           </svg>
         </div>
       </header >
