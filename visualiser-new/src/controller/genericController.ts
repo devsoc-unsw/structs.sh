a4fbc9cb884beba177c3912bb41d9aa2ebb8ad63
JosephXu741 <joseph499@hotmail.com>
2021-09-17 18:43:56 +1000

    changed folder names and implemented slider for animation

---
+++ visualiser-new/src/controller/genericController.ts
@@ -0,0 +1,57 @@
+import anime, { AnimeTimelineInstance } from 'animejs';
+import { Animation } from '../linked-list-visualiser/typedefs';
+
+// controls todo:
+// [x] play/pause
+// [ ] step to the next or previous timestamp in the current timeline
+// [ ] run a sequence in step mode or sequential mode
+// [ ] control to skip the animation of a sequence
+
+// eventually this file should be placed in a folder common for all data structures,
+// not just for the linked list
+class AnimationController {
+    private currentTimeline: AnimeTimelineInstance = anime.timeline();
+    private timelineHistory: AnimeTimelineInstance[] = [];
+    private timelineIndex: number = 0;
+
+    public play(): void {
+        this.currentTimeline.play();
+    }
+
+    public pause(): void {
+        console.log(this.currentTimeline);
+        this.currentTimeline.pause();
+    }
+
+    public seek(position: number): void {
+        this.currentTimeline.seek(this.currentTimeline.duration * (position / 100))
+    }
+
+    // this function runs a sequence of animations sequentially
+    // when stepSequence = false or pauses the timeline after each animation finishes
+    public runSequeuce(sequence: Animation[], slider: HTMLInputElement): void {
+        console.log(this);
+        this.currentTimeline = anime.timeline({
+            duration: 700,
+            easing: 'easeOutExpo',
+            update: function(anim) {
+                slider.value = String(anim.progress);
+              }
+        });
+
+        for (const seq of sequence) {
+            this.currentTimeline.add(seq);
+        }
+
+        this.timelineHistory.push(this.currentTimeline);
+        this.timelineIndex++;
+    }
+
+    public runNextSequence(): void {
+        if (this.timelineIndex === this.timelineHistory.length - 1) {
+            console.log('cant run next sequence');
+        }
+    }
+}
+
+export default AnimationController;
