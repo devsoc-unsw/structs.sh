11838bbf3c50a4f6bc26a35c01c76dd348ce50bb
achandra22 <achandra220@gmail.com>
2022-02-21 15:05:07 +1100

    used commandInputRules to validate argument length

--- client/src/components/Visualiser/executableCommands.ts
+++ client/src/components/Visualiser/executableCommands.ts
@@ -1,83 +1,80 @@
 // for documentation read: https://compclub.atlassian.net/wiki/spaces/S/pages/2150892071/Documentation#Visualiser-Docs%3A
 
-import { getDocumentation, getGUICommands } from './commandsInputRules';
+import { getDocumentation, getGUICommands } from "./commandsInputRules";
 
-const getLinkedListExecutor =
-  (visualiser, updateTimeline) =>
-  (command: string, args: string[]): string => {
-    console.log(command, args);
-    if (!isValidCommandArgs(command, args, 'Linked Lists')) {
-      const usage = getDocumentation('Linked Lists').find(
-        (operation) => operation.command == command
-      ).usage;
-      return `Invalid arguments. Usage: ${usage}`;
-    }
-    switch (command) {
-      case 'append':
-        visualiser.appendNode(Number(args[0]), updateTimeline);
-        break;
-      case 'delete':
-        visualiser.deleteNode(Number(args[0]), updateTimeline);
-        break;
-      case 'insert':
-        visualiser.insertNode(Number(args[0]), Number(args[1]), updateTimeline);
-        break;
-      case 'search':
-        visualiser.searchList(Number(args[0]), updateTimeline);
-        break;
-      default:
-        return `Invalid command: ${command}`;
-    }
-    return '';
-  };
 
-const getBSTExecutor =
-  (visualiser, updateTimeline) =>
-  (command: string, args: string[]): string => {
-    if (!isValidCommandArgs(command, args, 'Binary Search Trees')) {
-      const usage = getDocumentation('Binary Search Trees').find(
-        (operation) => operation.command == command
-      ).usage;
-      return `Invalid arguments. Usage: ${usage}`;
-    }
-    switch (command) {
-      case 'insert':
-        visualiser.insert(Number(args[0]), updateTimeline);
-        break;
-      case 'rotateLeft':
-        visualiser.rotateLeft(Number(args[0]), updateTimeline);
-        break;
-      case 'rotateRight':
-        visualiser.rotateRight(Number(args[0]), updateTimeline);
-        break;
-      case 'inorderTraversal':
-        visualiser.inorderTraversal(updateTimeline);
-        break;
-      case 'preorderTraversal':
-        visualiser.preorderTraversal(updateTimeline);
-        break;
-      case 'postorderTraversal':
-        visualiser.postorderTraversal(updateTimeline);
-        break;
-      default:
-        return `Invalid command: ${command}`;
-    }
-    return '';
-  };
+const getLinkedListExecutor = (
+  visualiser,
+  updateTimeline,
+) => (command: string, args: string[]): string => {
+  console.log(command, args)
+  if (!isValidCommandArgs(command, args, 'Linked Lists')) {
+    const usage = getDocumentation('Linked Lists')
+                    .find(operation => operation.command == command).usage
+    return `Invalid arguments. Usage: ${usage}`;;
+  }
+  switch (command) {
+    case 'append':
+      visualiser.appendNode(Number(args[0]), updateTimeline);
+      break;
+    case 'delete':
+      visualiser.deleteNode(Number(args[0]), updateTimeline);
+      break;
+    case 'insert':
+      visualiser.insertNode(Number(args[0]), Number(args[1]), updateTimeline);
+      break;
+    case 'search':
+      visualiser.searchList(Number(args[0]), updateTimeline);
+      break;
+    default:
+      return `Invalid command: ${command}`;
+  }
+  return '';
+};
 
-const undefinedExecutor =
-  (topicTitle) =>
-  (command: string, args: string[]): string => {
-    console.error(`Can't find the executor for ${topicTitle}`);
-    return '';
-  };
+const getBSTExecutor = (
+  visualiser,
+  updateTimeline,
+) => (command: string, args: string[]): string => {
+  if (!isValidCommandArgs(command, args, 'Binary Search Trees')) {
+    const usage = getDocumentation('Binary Search Trees')
+                    .find(operation => operation.command == command).usage
+    return `Invalid arguments. Usage: ${usage}`;;
+  }
+  switch (command) {
+    case 'insert':
+      visualiser.insert(Number(args[0]), updateTimeline);
+      break;
+    case 'rotateLeft':
+      visualiser.rotateLeft(Number(args[0]), updateTimeline);
+      break;
+    case 'rotateRight':
+      visualiser.rotateRight(Number(args[0]), updateTimeline);
+      break;
+    case 'inorderTraversal':
+      visualiser.inorderTraversal(updateTimeline);
+      break;
+    case 'preorderTraversal':
+      visualiser.preorderTraversal(updateTimeline);
+      break;
+    case 'postorderTraversal':
+      visualiser.postorderTraversal(updateTimeline);
+      break;
+    default:
+      return `Invalid command: ${command}`;
+  }
+  return '';
+};
+
+const undefinedExecutor = (topicTitle) => (command: string, args: string[]): string => {
+  console.error(`Can't find the executor for ${topicTitle}`);
+  return '';
+};
 
 const isValidCommandArgs = (command: string, args: string[], topicTitle: string): boolean => {
-  const currentOperation = getGUICommands(topicTitle).find(
-    (operation) => operation.command === command
-  );
+  const currentOperation = getGUICommands(topicTitle).find(operation => operation.command === command)
   return args.length === currentOperation.args.length && args.every((value) => /^\d+$/.test(value));
-};
+}
 
 const getCommandExecutor = (topicTitle, visualiser, updateTimeline) => {
   switch (topicTitle) {
