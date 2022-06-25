8b93181b0661a1752d6eb936a0b591be491d18fc
Joanna Lin <linjoanna31@gmail.com>
2022-06-25 18:31:52 +1000

    Got rid of db dependency. Changed sorting image to blocks

--- client/src/components/Topics/TopicCard.tsx
+++ client/src/components/Topics/TopicCard.tsx
@@ -4,26 +4,35 @@ import { Topic } from 'utils/apiRequests';
 import { useNavigate } from 'react-router-dom';
 import { titleToUrl } from 'utils/url';
 import styles from './TopicCard.module.scss';
-import binary from './topic-images/binary-search-tree-nobg.png'
+import binary from './topic-images/binary-search-tree-nobg.png';
 import linked from './topic-images/linked-list-nobg.png';
 import graph from './topic-images/graph-nobg.png';
-import sort from './topic-images/sort-nobg.png';
+import sort from './topic-images/sorting-nobg.png';
 
 interface Props {
-  topic: Topic;
+  topic: string;
   index: number;
 }
 
-const images = [linked, binary, binary, sort];
-const colours = ['rgba(248, 79, 121, 1)', 'rgba(20, 201, 150, 1)', 'rgba(120, 110, 243, 1)', 'rgba(76, 201, 240, 1)'];
-const coloursFaded = ['rgba(248, 79, 121, 0.8)', 'rgba(20, 201, 150, 0.8)', 'rgba(120, 110, 243, 0.8)', 'rgba(76, 201, 240, 0.8)'];
+const images = [linked, binary, sort, binary];
+const colours = [
+  'rgba(248, 79, 121, 1)',
+  'rgba(20, 201, 150, 1)',
+  'rgba(120, 110, 243, 1)',
+  'rgba(76, 201, 240, 1)',
+];
+const coloursFaded = [
+  'rgba(248, 79, 121, 0.8)',
+  'rgba(20, 201, 150, 0.8)',
+  'rgba(120, 110, 243, 0.8)',
+  'rgba(76, 201, 240, 0.8)',
+];
 
 const TopicCard: React.FC<Props> = ({ topic, index }) => {
-
   const navigate = useNavigate();
 
-  const handleClick = (top: Topic) => {
-    navigate(`/visualiser/${titleToUrl(top.title)}`);
+  const handleClick = (top: string) => {
+    navigate(`/visualiser/${titleToUrl(top)}`);
   };
 
   return (
@@ -32,26 +41,21 @@ const TopicCard: React.FC<Props> = ({ topic, index }) => {
       style={{
         background: `linear-gradient(to bottom right, ${colours[index]}, ${coloursFaded[index]})`,
       }}
-      role = "button"
+      role="button"
       tabIndex={index}
       onClick={() => handleClick(topic)}
       onKeyDown={() => handleClick(topic)}
     >
-      <img 
-        src={`${images[index]}`} 
-        alt={`${topic.title} svg snapshot`}
-        style={{height: '100px', objectFit: 'contain'}}
+      <img
+        src={`${images[index]}`}
+        alt={`${topic} svg snapshot`}
+        style={{ height: '100px', objectFit: 'contain' }}
       />
-      <Button
-        className={`${styles.button}`}
-        id={styles[`button${index}`]}
-        variant="contained"
-      >
-        {topic.title}
+      <Button className={`${styles.button}`} id={styles[`button${index}`]} variant="contained">
+        {topic}
       </Button>
     </div>
   );
-  
-}
+};
 
 export default TopicCard;
--- client/src/components/Topics/Topics.tsx
+++ client/src/components/Topics/Topics.tsx
@@ -1,24 +1,25 @@
+import React from 'react';
 import { LineLoader } from 'components/Loader';
-import React, { useEffect, useState } from 'react';
-import { getTopics, Topic } from 'utils/apiRequests';
-import Notification from 'utils/Notification';
+import { getTopics } from '../../visualiser-src/common/helpers';
 import TopicCard from './TopicCard';
 
 interface Props {}
 
 const Topics: React.FC<Props> = () => {
-
-  const [topics, setTopics] = useState<Topic[]>([]);
-  
-  useEffect(() => {
-    getTopics().then(setTopics).catch(Notification.error);
-  }, []);
-
+  const topics = getTopics();
   return topics && topics.length > 0 ? (
     <div
-      style={{display: 'flex', flexWrap: 'wrap', flexDirection: 'row', justifyContent: 'center', marginTop: '5%', gap: '20px 50px'}}>
+      style={{
+        display: 'flex',
+        flexWrap: 'wrap',
+        flexDirection: 'row',
+        justifyContent: 'center',
+        marginTop: '5%',
+        gap: '20px 50px',
+      }}
+    >
       {topics.map((topic, idx) => (
-        <TopicCard topic={topic} index={idx} key={idx}/>
+        <TopicCard topic={topic} index={idx} key={idx} />
       ))}
     </div>
   ) : (
