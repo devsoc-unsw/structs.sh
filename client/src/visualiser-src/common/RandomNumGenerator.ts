c36f3c13e10f027726618c921a64314d17ad1c69
trollic <cece.jzhu@gmail.com>
2022-06-27 17:10:10 +1000

    formatting and style fixes

--- client/src/visualiser-src/common/RandomNumGenerator.ts
+++ client/src/visualiser-src/common/RandomNumGenerator.ts
@@ -1,12 +1,12 @@
 // generates a random list of unique numbers
 // min and max represent the range of total nodes to be be created
 export const generateNumbers = () =>{
-  var result = [];
-  var set = new Set();
+  const result = [];
+  const set = new Set();
   const min = 4;
   const max = 9;
   const totalNodes = Math.floor(Math.random() * (max - min + 1) + min);
-  for (let i = 0; i < totalNodes; i++) {
+  for (let i = 0; i < totalNodes; i += 1) {
     const num = Math.floor(Math.random() * 10);
     set.add(num);
   }
@@ -19,9 +19,7 @@ export const generateNumbers = () =>{
 // will lead to a reasonably balanced BST
 function recurseArrInsert(arr, start, end, num): void {
   // the base case is array length <=1
-  if (end - start === 0) {
-    return;
-  } else {
+  if (end - start !== 0) {
     // insert midpoint
     const mid = Math.floor((end + start)/ 2);
     num.push(arr[mid]);
@@ -35,8 +33,8 @@ function recurseArrInsert(arr, start, end, num): void {
 
 export const insertBalancedBSTNumbers = () => {
   console.log("insertBalancedBSTNumbers");
-  var arr = generateNumbers().sort();
-  var num = [];
+  const arr = generateNumbers().sort();
+  const num = [];
   recurseArrInsert(arr, 0, arr.length, num);
   console.log(num);
   return num; 
