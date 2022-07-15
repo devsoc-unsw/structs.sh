0f91072fb94d30a544fa28c3c2d5de58c7340af6
Justin <95273765+Kluvens@users.noreply.github.com>
2022-07-15 17:14:15 +1000

    Update AnimationProducer.ts
    add .x back

--- client/src/visualiser-src/common/AnimationProducer.ts
+++ client/src/visualiser-src/common/AnimationProducer.ts
@@ -65,6 +65,7 @@ export default abstract class AnimationProducer {
           .fill('#FFFFFF')
           .attr('style', 'white-space: pre-wrap')
           .move(0, 18 * i + 2)
+          .x(line.search(/\S/) > 0 ? line.search(/\S/) * 5 : 0)
           .addTo(CODE_CANVAS),
       };
 
