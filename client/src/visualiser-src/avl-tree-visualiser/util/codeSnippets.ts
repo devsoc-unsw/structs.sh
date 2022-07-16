3f98e7e3068b46e003822ebb612977cff017e2d0
jedwed <83894732+jedwed@users.noreply.github.com>
2022-07-16 18:30:35 +1000

    AVL code snippets highlight correct lines, rectangle height resized to 18

--- client/src/visualiser-src/avl-tree-visualiser/util/codeSnippets.ts
+++ client/src/visualiser-src/avl-tree-visualiser/util/codeSnippets.ts
@@ -1,7 +1,6 @@
 export const insertCodeSnippet = `struct node *insert(struct node *node, int value) {
     if (node == null)
         return create_new_node(value);
-
     if (value < node->value)
         node->left = insert(node->left, value);
     if (value > node->value)
@@ -12,14 +11,12 @@ export const insertCodeSnippet = `struct node *insert(struct node *node, int val
     node->height = height(node);
     int balance = height(node->left) - height(node->right);
     if (balance > 1) {
-        if (value > node->left->value) {
+        if (value > node->left->value) 
             node->left = rotate_left(node->left);
-        }
         return rotate_right(node);
     } else if (balance < -1) {
-        if (value < node->right->value) {
+        if (value < node->right->value) 
             node->right = rotate_right(node->right);
-        }
         return rotate_left(node);
     } else {
         return node;
