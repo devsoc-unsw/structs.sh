89d505b94400495c83d0843d11e95737b1334d5c
jedwed <83894732+jedwed@users.noreply.github.com>
2022-07-20 23:56:11 +1000

    Topic title now displayed in TopNavbar

--- client/src/components/Navbars/TopNavbar.tsx
+++ client/src/components/Navbars/TopNavbar.tsx
@@ -1,27 +1,19 @@
-import AccountCircle from '@mui/icons-material/AccountCircle';
-import SunIcon from '@mui/icons-material/Brightness7';
-import FacebookIcon from '@mui/icons-material/Facebook';
-import GoogleIcon from '@mui/icons-material/Google';
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
-import MoreIcon from '@mui/icons-material/MoreVert';
-import MoonIcon from '@mui/icons-material/NightsStay';
-import { Button, FormControl, TextField, useTheme } from '@mui/material';
+import { Button, FormControl, ListItemIcon, TextField, useTheme } from '@mui/material';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
-import IconButton from '@mui/material/IconButton';
 import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import { SxProps } from '@mui/system';
+import ListItemText from '@mui/material/ListItemText';
+import Check from '@mui/icons-material/Check';
 // import { ThemeMutationContext } from 'App';
 import logo from 'assets/img/logo.png';
 import React, { FC, useContext, useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
-import Notification from 'utils/Notification';
-import { titleToUrl } from 'utils/url';
+import { Link, useParams } from 'react-router-dom';
+import { titleToUrl, toTitleCase, urlToTitle } from 'utils/url';
 import { getTopics } from '../../visualiser-src/common/helpers';
-import Drawer from './Drawer';
 // import SidebarContents from './SidebarContents';
 import styles from './TopNavbar.module.scss';
 
@@ -33,29 +25,11 @@ interface Props {
 const TopNavbar: FC<Props> = ({ position = 'fixed', enableOnScrollEffect = true }) => {
   const theme = useTheme();
 
-  const [hasScrolledDown, setHasScrolledDown] = useState<boolean>(false);
-
-  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
   const [learnAnchorEl, setLearnAnchorEl] = React.useState<null | HTMLElement>(null);
-  const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState<null | HTMLElement>(null);
 
-  const isMenuOpen = Boolean(anchorEl);
   const isLearnMenuOpen = Boolean(learnAnchorEl);
-  const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
 
-  /* -------------------------- Page Scroll Callbacks ------------------------- */
-
-  const detectUserHasScrolledDown = () => {
-    const yOffsetPx: number = Number(window.pageYOffset);
-    setHasScrolledDown(!(yOffsetPx <= 0));
-  };
-
-  useEffect(() => {
-    window.addEventListener('scroll', detectUserHasScrolledDown);
-    return () => {
-      window.removeEventListener('scroll', detectUserHasScrolledDown);
-    };
-  }, [setHasScrolledDown]);
+  const currTopic = toTitleCase(urlToTitle(useParams().topic || ''));
 
   /* ------------------------------ Data Fetching ----------------------------- */
 
@@ -66,23 +40,6 @@ const TopNavbar: FC<Props> = ({ position = 'fixed', enableOnScrollEffect = true 
   // }, []);
 
   /* --------------------------- Dropdown Callbacks --------------------------- */
-
-  const handleProfileMenuOpen = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorEl(event.currentTarget);
-  };
-
-  const handleMobileMenuClose = () => {
-    setMobileMoreAnchorEl(null);
-  };
-
-  const handleMenuClose = () => {
-    setAnchorEl(null);
-    handleMobileMenuClose();
-  };
-
-  const handleMobileMenuOpen = (event: React.MouseEvent<HTMLElement>) => {
-    setMobileMoreAnchorEl(event.currentTarget);
-  };
 
   const handleLearnMenuOpen = (event: React.MouseEvent<HTMLButtonElement>) => {
     setLearnAnchorEl(event.currentTarget);
@@ -112,7 +69,16 @@ const TopNavbar: FC<Props> = ({ position = 'fixed', enableOnScrollEffect = true 
             to={`/visualiser/${titleToUrl(topic)}`}
             onClick={handleLearnMenuClose}
           >
-            {topic}
+            {topic.toLowerCase() === currTopic.toLowerCase() ? (
+              <>
+                <ListItemIcon>
+                  <Check sx={{ fill: 'white' }} />
+                </ListItemIcon>
+                <ListItemText>{topic}</ListItemText>
+              </>
+            ) : (
+              <ListItemText inset>{topic}</ListItemText>
+            )}
           </MenuItem>
         ))}
     </Menu>
@@ -122,7 +88,6 @@ const TopNavbar: FC<Props> = ({ position = 'fixed', enableOnScrollEffect = true 
     <Box>
       <AppBar
         position={position}
-        elevation={0}
         sx={{
           transition: '0.5s all ease-in-out',
           backgroundColor: theme.palette.background.default,
@@ -132,7 +97,7 @@ const TopNavbar: FC<Props> = ({ position = 'fixed', enableOnScrollEffect = true 
           <Box display="flex" alignItems="center" width="100%">
             <Button color="info" onClick={handleLearnMenuOpen} endIcon={<KeyboardArrowDownIcon />}>
               <Typography>
-                <strong>Topics</strong>
+                Topic: <strong>{currTopic}</strong>
               </Typography>
             </Button>
             <Box className={styles.centralBox}>
