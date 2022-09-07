e060f8c046fb78976da4ac0fdbab996154b897de
trollic <33384880+trollic@users.noreply.github.com>
2022-09-07 13:19:14 +1000

    Cz/refactor placement of create new reset buttons (#345)
    * moved the 'Create New'/'Reset' button to float at the top right, and moved linked list nodes down to prevent visual conflict

---
+++ client/src/components/Visualiser/Controller/VisualiserCreateNewReset.tsx
@@ -0,0 +1,50 @@
+import {
+  Box,
+  Typography,
+  useTheme,
+  Button,
+} from '@mui/material';
+import React, { useCallback, useContext } from 'react';
+import VisualiserContext from '../VisualiserContext';
+
+import styles from './Control.module.scss';
+
+/**
+ * Contains all the visualiser controller UI, ie. the play/pause buttons, the
+ * sliders, etc.
+ *
+ * It receives a bunch of callbacks and connects it to each of the corresponding
+ * UI components.
+ *
+ * Eg. it receives a `handlePlay` callback and attaches it to the Play button's
+ *     `onClick` handler.
+ */
+const VisualiserCreateNewReset = () => {
+  const { controller } = useContext(VisualiserContext);
+  const theme = useTheme();
+
+  const handleReset = useCallback(() => {
+    controller.resetDataStructure();
+  }, [controller]);
+
+  const handleGenerate = useCallback(() => {
+    controller.generateDataStructure();
+  }, [controller]);
+
+  return (
+    <Box className={styles.createResetMenu}>
+      <Button className={styles.resetButton} onClick={handleGenerate}  sx={{ backgroundColor: '#46B493' }}>
+        <Typography color="textPrimary" sx={{ whiteSpace: 'nowrap' }}>
+          Create New
+        </Typography>
+      </Button>
+      <Button className={styles.resetButton} onClick={handleReset} sx={{ backgroundColor: '#46B493' }}>
+        <Typography color="textPrimary" whiteSpace="nowrap">
+          Reset All
+        </Typography>
+      </Button>
+    </Box>
+  );
+};
+
+export default VisualiserCreateNewReset;
