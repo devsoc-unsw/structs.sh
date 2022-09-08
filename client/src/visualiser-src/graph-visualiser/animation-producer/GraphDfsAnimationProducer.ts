fc82774e6cbeeeca0e42aa1079de2b59e2efa776
Tim Zhang <timzhang3@gmail.com>
2022-09-08 17:20:10 +1000

    Undo linting to .eslintrc.json and remove incorrect comment from GraphDfsAnimationProducer.ts

--- client/src/visualiser-src/graph-visualiser/animation-producer/GraphDfsAnimationProducer.ts
+++ client/src/visualiser-src/graph-visualiser/animation-producer/GraphDfsAnimationProducer.ts
@@ -1,7 +1,6 @@
 import { dfsCodeSnippet } from '../util/codeSnippets';
 import GraphAnimationProducer from './GraphAnimationProducer';
 
-// Class that produces SVG.Runners animating linked list operations specific to appending
 export default class GraphDfsAnimationProducer extends GraphAnimationProducer {
   public renderDfsCode() {
     this.renderCode(dfsCodeSnippet);
