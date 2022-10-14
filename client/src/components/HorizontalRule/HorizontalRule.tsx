0ff0935f1734b3774f53c83b01e61213f3271da2
jedwed <83894732+jedwed@users.noreply.github.com>
2022-10-14 16:42:33 +1100

    added sx back to divider so it appears more clearly

--- client/src/components/HorizontalRule/HorizontalRule.tsx
+++ client/src/components/HorizontalRule/HorizontalRule.tsx
@@ -3,6 +3,9 @@ import Divider from '@mui/material/Divider';
 import { useTheme } from '@mui/system';
 import React from 'react';
 
-const HorizontalRule = () => <Divider light sx={{ mt: 2, mb: 2 }} />;
+const HorizontalRule = () => {
+  const theme = useTheme();
+  return <Divider sx={{ mt: 2, mb: 2, background: theme.palette.text.primary }} />;
+};
 
 export default HorizontalRule;
