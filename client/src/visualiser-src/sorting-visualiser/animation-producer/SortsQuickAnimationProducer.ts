21e740764d44ec37b1a1eb8262800b0f4bd8789c
Jin Sunwoo <jin.sunwoo@csesoc.org.au>
2022-10-14 16:14:05 +1100

    Quick Sort code highlighting

--- client/src/visualiser-src/sorting-visualiser/animation-producer/SortsQuickAnimationProducer.ts
+++ client/src/visualiser-src/sorting-visualiser/animation-producer/SortsQuickAnimationProducer.ts
@@ -3,7 +3,7 @@ import { quickCodeSnippet } from '../util/codeSnippets';
 import { getX, getCx } from '../util/helpers';
 import GraphicalSortsElement from '../data-structure/GraphicalSortsElement';
 import SortsAnimationProducer from './SortsAnimationProducer';
-import { boxWidth, defaultColour, sortedColour } from '../util/constants';
+import { boxWidth, checkingColour, defaultColour, sortedColour } from '../util/constants';
 
 export default class SortsQuickAnimationProducer extends SortsAnimationProducer {
     public renderQuickCode() {
