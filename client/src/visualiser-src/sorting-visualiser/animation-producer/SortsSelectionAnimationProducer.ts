e65f12fffd161382fa09dbb857f57e0ca3b651fa
jedwed <83894732+jedwed@users.noreply.github.com>
2022-10-28 18:58:18 +1100

    Eq/selection sort (#366)
    * Swapping complete, need to highlight and animate correctly
    
    * Selection sort complete
    
    * Changed colors, highlighted the line for when i changes in the for loop
    
    * Made highlighting ith element and swapping one sequence, changed highlight color of ith element
    
    * Moved the unhighlight buffer method into a class in the animation producer exercising move method gg
    
    * Changed variable names
    
    * Removed fade in and fade out

---
+++ client/src/visualiser-src/sorting-visualiser/animation-producer/SortsSelectionAnimationProducer.ts
@@ -0,0 +1,88 @@
+import { selectionCodeSnippet } from '../util/codeSnippets';
+import { getX, getCx } from '../util/helpers';
+import GraphicalSortsElement from '../data-structure/GraphicalSortsElement';
+import SortsAnimationProducer from './SortsAnimationProducer';
+import {
+  defaultColour,
+  sortedColour,
+  checkingColour,
+  comparingColor,
+  selectedColor,
+} from '../util/constants';
+
+export default class SortsSelectionAnimationProducer extends SortsAnimationProducer {
+  public renderSelectionCode() {
+    this.renderCode(selectionCodeSnippet);
+  }
+
+  public check(currItem: GraphicalSortsElement) {
+    this.addSequenceAnimation(currItem.boxTarget.animate(1).attr({ stroke: comparingColor }));
+    this.addSequenceAnimation(currItem.boxTarget.animate(1).attr({ fill: comparingColor }));
+    this.addSequenceAnimation(currItem.numberTarget.animate(1).attr({ fill: comparingColor }));
+    this.addSequenceAnimation(currItem.numberTarget.animate());
+    this.finishSequence();
+  }
+
+  public select(newMinItem: GraphicalSortsElement, previousMinItem: GraphicalSortsElement) {
+    this.addSequenceAnimation(previousMinItem.boxTarget.animate(1).attr({ stroke: defaultColour }));
+    this.addSequenceAnimation(previousMinItem.boxTarget.animate(1).attr({ fill: defaultColour }));
+    this.addSequenceAnimation(
+      previousMinItem.numberTarget.animate(1).attr({ fill: defaultColour })
+    );
+    this.addSequenceAnimation(newMinItem.boxTarget.animate(1).attr({ stroke: selectedColor }));
+    this.addSequenceAnimation(newMinItem.boxTarget.animate(1).attr({ fill: selectedColor }));
+    this.addSequenceAnimation(newMinItem.numberTarget.animate(1).attr({ fill: selectedColor }));
+    // Empty animation so that there's a pause
+    this.addSequenceAnimation(newMinItem.numberTarget.animate());
+    this.finishSequence();
+  }
+
+  public selectionSwap(
+    iItem: GraphicalSortsElement,
+    iIndex: number,
+    minItem: GraphicalSortsElement,
+    minIndex: number
+  ) {
+    if (iIndex !== minIndex) {
+      this.addSequenceAnimation(iItem.boxTarget.animate(1).attr({ stroke: selectedColor }));
+      this.addSequenceAnimation(iItem.boxTarget.animate(1).attr({ fill: selectedColor }));
+      this.addSequenceAnimation(iItem.numberTarget.animate(1).attr({ fill: selectedColor }));
+      // Empty animation so that there's a pause
+      this.addSequenceAnimation(iItem.numberTarget.animate());
+    }
+    this.swap(iItem, iIndex, minItem, minIndex);
+  }
+
+  public finishSelectionRound(
+    newIthItem: GraphicalSortsElement,
+    prevIthItem: GraphicalSortsElement,
+    ithItemChanged: boolean
+  ) {
+    this.addSequenceAnimation(newIthItem.boxTarget.animate(1).attr({ stroke: sortedColour }));
+    this.addSequenceAnimation(newIthItem.boxTarget.animate(1).attr({ fill: sortedColour }));
+    this.addSequenceAnimation(newIthItem.numberTarget.animate(1).attr({ fill: sortedColour }));
+    this.addSequenceAnimation(newIthItem.numberTarget.animate());
+    if (ithItemChanged) {
+      this.addSequenceAnimation(prevIthItem.boxTarget.animate(1).attr({ stroke: defaultColour }));
+      this.addSequenceAnimation(prevIthItem.boxTarget.animate(1).attr({ fill: defaultColour }));
+      this.addSequenceAnimation(prevIthItem.numberTarget.animate(1).attr({ fill: defaultColour }));
+    }
+  }
+
+  public bufferUnhighlight(prevMin: GraphicalSortsElement) {
+    this.addSequenceAnimation(prevMin.boxTarget.animate(1).attr({ stroke: defaultColour }));
+    this.addSequenceAnimation(prevMin.boxTarget.animate(1).attr({ fill: defaultColour }));
+    this.addSequenceAnimation(prevMin.numberTarget.animate(1).attr({ fill: defaultColour }));
+  }
+
+  public highlightAll(array: GraphicalSortsElement[], color: string) {
+    array.forEach((item) => {
+      this.addSequenceAnimation(item.boxTarget.animate(1).attr({ stroke: color }));
+      this.addSequenceAnimation(item.boxTarget.animate(1).attr({ fill: color }));
+      this.addSequenceAnimation(item.numberTarget.animate(1).attr({ fill: color }));
+      // Empty animation so that there's a pause
+      this.addSequenceAnimation(item.numberTarget.animate());
+    });
+    this.finishSequence();
+  }
+}
