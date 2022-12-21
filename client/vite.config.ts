1081208c45694456ed421b98968aa689658c4a0d
jedwed <83894732+jedwed@users.noreply.github.com>
2022-12-21 21:04:12 +1100

    Modified vite config to build to build directory + start server at port 3000

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -6,4 +6,6 @@ import checker from 'vite-plugin-checker';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths(), checker({ typescript: true })],
+  build: { outDir: 'build' },
+  server: { port: 3000 },
 });
