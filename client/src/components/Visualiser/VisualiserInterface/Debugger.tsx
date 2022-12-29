ac4af09d608b7069e391379a6eb17710e9fa67ce
jedwed <83894732+jedwed@users.noreply.github.com>
2022-12-29 11:06:09 +1100

    Store code in state, yeet minimap

--- client/src/components/Visualiser/VisualiserInterface/Debugger.tsx
+++ client/src/components/Visualiser/VisualiserInterface/Debugger.tsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react';
 import { Button, useTheme } from '@mui/material';
 import Editor, { loader } from '@monaco-editor/react';
 import monokai from 'monaco-themes/themes/Monokai.json';
@@ -7,12 +8,65 @@ import Console from './Console';
 import BuildIcon from '@mui/icons-material/Build';
 import PlayArrowIcon from '@mui/icons-material/PlayArrow';
 
+const program = `#include <stdio.h>
+#include <stdlib.h>
+
+struct list {
+    struct node *head;
+};
+
+struct node {
+    int data;
+    struct node *next; 
+};
+
+struct node *create_new_node(int data) {
+    struct node *new = malloc(sizeof(struct node));
+    new->data = data;
+    new->next = NULL;
+    return new;
+}
+
+void append(int value, struct list *list) {
+    struct node *new_tail = create_new_node(value);
+    if (list->head == NULL) {
+        list->head = new_tail;
+        return;
+    }
+
+    struct node *curr = list->head;
+    while (curr->next != NULL) {
+        curr = curr->next;
+    }
+
+    curr->next = new_tail
+};
+
+
+int main(int argc, char *argv[]) {
+    // Declare pointer to the head of the linked list, which is empty
+    struct list *list = malloc(sizeof(struct list));
+    list->head = NULL;
+    append(5, list);
+    append(10, list);
+}
+`;
+
 const Debugger = () => {
+  const [code, setCode] = useState(program);
   const theme = useTheme();
-  loader.init().then((monaco) => {
-    monaco.editor.defineTheme('monokai', monokai);
-    monaco.editor.defineTheme('githubDark', githubDark);
-  });
+  const handleEditorChange = (value: string) => {
+    // setValue(value);
+    setCode(value);
+    console.log(value);
+    console.log(code);
+  };
+  useEffect(() => {
+    loader.init().then((monaco) => {
+      // monaco.editor.defineTheme('monokai', monokai);
+      monaco.editor.defineTheme('githubDark', githubDark);
+    });
+  }, []);
   return (
     <Box
       // position="absolute"
@@ -36,7 +90,18 @@ const Debugger = () => {
         </Button>
       </Box>
       <Box display="flex" flexDirection="column">
-        <Editor language="c" theme="githubDark" height="60vh" />
+        <Editor
+          language="c"
+          theme="githubDark"
+          height="60vh"
+          options={{
+            minimap: {
+              enabled: false,
+            },
+          }}
+          value={code}
+          onChange={handleEditorChange}
+        />
         <Console />
       </Box>
     </Box>
