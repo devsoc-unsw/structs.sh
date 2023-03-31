816f3453596dee1ff4afaa62cb46f124d9aa0413
Jin Sunwoo <jin.sunwoo@csesoc.org.au>
2023-03-31 17:15:54 +1100

    renamed server.ts to index.ts and added mongodb connection

---
+++ server/src/index.ts
@@ -0,0 +1,20 @@
+import express from 'express'
+import mongoose, { ConnectOptions } from 'mongoose'
+import { json } from 'body-parser'
+import { todoRouter } from './routes/todo'
+
+const app = express()
+
+app.use(json())
+app.use(todoRouter)
+
+const connectionString = 'mongodb+srv://jinsunwoo:b6M4MmX5x6gt3y7l@structsdb.1rge7z4.mongodb.net/?retryWrites=true&w=majority'
+
+mongoose.connect(connectionString).then(
+    () => { console.log("Connected To Database") },
+    err => { console.log("Could Not Connect to Database") }
+)
+
+app.listen(3000, () => {
+    console.log("Listening on 3000")
+})
