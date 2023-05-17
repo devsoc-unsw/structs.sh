d5e3326c69873e88f8eb8995176084254bc4d73d
Audrey Tanama <z5362329@ad.unsw.edu.au>
2023-05-17 17:14:05 +1000

    created console

--- client/src/components/DevelopmentMode/CodeEditor.tsx
+++ client/src/components/DevelopmentMode/CodeEditor.tsx
@@ -4,32 +4,88 @@ import CodeMirror from '@uiw/react-codemirror';
 import { cpp } from '@codemirror/lang-cpp';
 import Button from '@mui/material/Button';
 import SendIcon from '@mui/icons-material/Send';
+import { TopNavbar } from 'components/Navbars';
+import TextField from '@mui/material/TextField';
+import Box from '@mui/material/Box';
 
 const CodeEditor: FC = () => {
   const placeholder = "// Code your stuff below!";
   const [code, setCode] = React.useState(placeholder);
+  const [stdout, setStdout] = React.useState('');
+  const [input, setInput] = React.useState('');
 
-  const onChange = React.useCallback((value) => {
+  const onEditorChange = React.useCallback((value) => {
     console.log('value:', value);
     setCode(value);
   }, []);
 
-  const sendCode = ( ) => {
+  const onInputChange = React.useCallback((event) => {
+    console.log('Input:', event.target.value);
+    setInput(event.target.value);
+  }, []);
+
+  const runCode = ( ) => {
     console.log('codeSent:', code);
+
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ language: 'C', code: code })
+    };
+
+    fetch('https://online-debugger-api.onrender.com/compile', requestOptions)
+        .then(response => response.json())
+        .then(data => setStdout(data.stdout));
+    
+    console.log(stdout);
   }
 
 	return (
 		<>
-			<div>Hello, world!</div>
-			<CodeMirror
-        value={placeholder}
-        height="200px"
-        extensions={[cpp()]}
-        onChange={onChange}
-      />
-      <Button onClick={sendCode} variant="contained" endIcon={<SendIcon />}>
-        Send Code
-      </Button>
+      <TopNavbar/>
+      <div style={{marginTop: '5rem', marginLeft: '1rem', marginRight: '1rem'}}>
+        <CodeMirror
+          value={placeholder}
+          height='200px'
+          extensions={[cpp()]}
+          onChange={onEditorChange}
+        />
+        <Button onClick={runCode} variant='contained' endIcon={<SendIcon />} style={{marginTop: '1rem'}}>
+          Run Code
+        </Button>
+
+        <TextField
+          fullWidth
+          label='Input'
+          multiline
+          rows={6}
+          placeholder='Type your input here..'
+          style={{display: 'block', marginTop: '3rem'}}
+          onChange={onInputChange}
+          value={input}
+        />
+        <Box
+        component="div"
+        sx={{
+          whiteSpace: 'nowrap',
+          overflowX: 'auto',
+          my: 2,
+          p: 1,
+          bgcolor: (theme) =>
+            theme.palette.mode === 'dark' ? '#101010' : 'grey.100',
+          color: (theme) =>
+            theme.palette.mode === 'dark' ? 'grey.300' : 'grey.800',
+          border: '1px solid',
+          borderColor: (theme) =>
+            theme.palette.mode === 'dark' ? 'grey.800' : 'grey.300',
+          borderRadius: 2,
+          fontSize: '0.875rem',
+          fontWeight: '700',
+        }}
+      >
+        Output: {stdout}
+      </Box>
+      </div>
 		</>
 	);
 };
