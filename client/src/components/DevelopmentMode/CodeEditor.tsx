d94e1c0f7c8d37184c9aa6ea4b28d060054afe89
Audrey Tanama <z5362329@ad.unsw.edu.au>
2023-05-23 21:17:11 +1000

    work on the history output

--- client/src/components/DevelopmentMode/CodeEditor.tsx
+++ client/src/components/DevelopmentMode/CodeEditor.tsx
@@ -13,13 +13,14 @@ const CodeEditor: FC = () => {
   const [code, setCode] = React.useState(placeholder);
   const [stdout, setStdout] = React.useState('');
   const [input, setInput] = React.useState('');
+  // const [history, setHistory] = React.useState('');
 
-  const onEditorChange = React.useCallback((value) => {
+  const onEditorChange = React.useCallback((value: React.SetStateAction<string>) => {
     console.log('value:', value);
     setCode(value);
   }, []);
 
-  const onInputChange = React.useCallback((event) => {
+  const onInputChange = React.useCallback((event: { target: { value: React.SetStateAction<string>; }; }) => {
     console.log('Input:', event.target.value);
     setInput(event.target.value);
   }, []);
@@ -35,7 +36,10 @@ const CodeEditor: FC = () => {
 
     fetch('https://online-debugger-api.onrender.com/compile', requestOptions)
         .then(response => response.json())
-        .then(data => setStdout(data.stdout));
+        .then(data => {
+          setStdout(data.stdout)
+          // setHistory(history + '\n' +  data.stdout)
+        });
     
     console.log(stdout);
   }
@@ -54,37 +58,63 @@ const CodeEditor: FC = () => {
           Run Code
         </Button>
 
+        {/* <TextField
+          disabled
+          fullWidth
+          multiline
+          rows={6}
+          style={{display: 'block', marginTop: '3rem'}}
+          onChange={onInputChange}
+          value={history}
+          onKeyPress={event => {
+            if (event.key === 'Enter') {
+              runCode();
+            }
+          }}
+          sx={{
+              "& .MuiInputBase-input.Mui-disabled": {
+                WebkitTextFillColor: "#000000",
+            },
+          }}
+        /> */}
+
         <TextField
           fullWidth
           label='Input'
           multiline
-          rows={6}
+          rows={4}
           placeholder='Type your input here..'
           style={{display: 'block', marginTop: '3rem'}}
           onChange={onInputChange}
           value={input}
+          onKeyPress={event => {
+            if (event.key === 'Enter') {
+              runCode();
+            }
+          }}
         />
         <Box
-        component="div"
-        sx={{
-          whiteSpace: 'nowrap',
-          overflowX: 'auto',
-          my: 2,
-          p: 1,
-          bgcolor: (theme) =>
-            theme.palette.mode === 'dark' ? '#101010' : 'grey.100',
-          color: (theme) =>
-            theme.palette.mode === 'dark' ? 'grey.300' : 'grey.800',
-          border: '1px solid',
-          borderColor: (theme) =>
-            theme.palette.mode === 'dark' ? 'grey.800' : 'grey.300',
-          borderRadius: 2,
-          fontSize: '0.875rem',
-          fontWeight: '700',
-        }}
-      >
-        Output: {stdout}
-      </Box>
+          component="div"
+          sx={{
+            whiteSpace: 'nowrap',
+            overflowX: 'auto',
+            my: 2,
+            p: 1,
+            bgcolor: (theme) =>
+              theme.palette.mode === 'dark' ? '#101010' : '#61729F',
+              // theme.palette.mode === 'dark' ? '#101010' : 'grey.100',
+            color: (theme) =>
+              theme.palette.mode === 'dark' ? 'grey.300' : '#FFFFFF',
+            border: '1px solid',
+            borderColor: (theme) =>
+              theme.palette.mode === 'dark' ? 'grey.800' : 'grey.300',
+            borderRadius: 2,
+            fontSize: '0.875rem',
+            fontWeight: '700',
+          }}
+        >
+          {/* Output: {history} */}
+        </Box>
       </div>
 		</>
 	);
