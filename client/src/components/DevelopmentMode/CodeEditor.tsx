c565f8f9b3bbfd8e6189e2256c78d49f09b5556a
Jonah Hopkin <z5419238@ad.unsw.edu.au>
2023-05-26 16:32:10 +1000

    Add code editor for side by side comparison

--- client/src/components/DevelopmentMode/CodeEditor.tsx
+++ client/src/components/DevelopmentMode/CodeEditor.tsx
@@ -4,6 +4,36 @@ import CodeMirror from '@uiw/react-codemirror';
 import { cpp } from '@codemirror/lang-cpp';
 import Button from '@mui/material/Button';
 import SendIcon from '@mui/icons-material/Send';
+import StackInspector from './StackInspector';
+
+// replace with websocket data once backend up and running
+const sampleData = [
+  {
+    name: 'a',
+    type: 'int',
+    value: '3',
+  },
+  {
+    name: 'c',
+    type: 'int',
+    value: '104',
+  },
+  {
+    name: 'b',
+    type: 'int [4]',
+    value: '{9, 6, 3, 2}',
+  },
+  {
+    name: 'foo',
+    type: 'int *',
+    value: '0x5555555592a0',
+  },
+  {
+    name: 'z',
+    type: 'int',
+    value: '0',
+  },
+];
 
 const CodeEditor: FC = () => {
   const placeholder = "// Code your stuff below!";
@@ -30,6 +60,7 @@ const CodeEditor: FC = () => {
       <Button onClick={sendCode} variant="contained" endIcon={<SendIcon />}>
         Send Code
       </Button>
+      <StackInspector debuggerData={sampleData} />
 		</>
 	);
 }
