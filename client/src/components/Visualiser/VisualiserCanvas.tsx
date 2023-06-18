ac800833baff7ba07abb7662f61644057be6272f
jedwed <83894732+jedwed@users.noreply.github.com>
2023-06-18 21:56:28 +1000

    Added scroll to zoom, currently uncentered and not smooth

--- client/src/components/Visualiser/VisualiserCanvas.tsx
+++ client/src/components/Visualiser/VisualiserCanvas.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Box } from '@mui/material';
 
 /* -------------------------------------------------------------------------- */
@@ -9,10 +9,19 @@ import { Box } from '@mui/material';
  * The React component that renders the DOM elements that the visualiser
  * attaches itself to.
  */
-const VisualiserCanvas: React.FC = () => (
-  <Box id="visualiser-container" margin="auto" width={window.screen.width}>
-    <svg id="visualiser-canvas" />
-  </Box>
-);
+const VisualiserCanvas: React.FC = () => {
+  const [scale, setScale] = useState(1);
+  const ZOOM_SPEED = 0.1;
+  const onScroll = (e: React.WheelEvent) => {
+    console.log(`scale: ${scale}`);
+    e.deltaY > 0 ? setScale(scale + ZOOM_SPEED) : setScale(scale - ZOOM_SPEED);
+  };
+
+  return (
+    <Box id="visualiser-container" margin="auto" width={window.screen.width}>
+      <svg transform={`scale(${scale})`} onWheel={onScroll} id="visualiser-canvas" />
+    </Box>
+  );
+};
 
 export default VisualiserCanvas;
