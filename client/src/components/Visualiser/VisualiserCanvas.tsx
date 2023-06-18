335bfd1f0bea2a1b34d753b6a19c4ed2c22527c3
jedwed <83894732+jedwed@users.noreply.github.com>
2023-06-18 22:15:14 +1000

    Set origin for transformations the center, reduced zoom speed

--- client/src/components/Visualiser/VisualiserCanvas.tsx
+++ client/src/components/Visualiser/VisualiserCanvas.tsx
@@ -11,15 +11,27 @@ import { Box } from '@mui/material';
  */
 const VisualiserCanvas: React.FC = () => {
   const [scale, setScale] = useState(1);
-  const ZOOM_SPEED = 0.1;
+  const ZOOM_SPEED = 0.05;
   const onScroll = (e: React.WheelEvent) => {
     console.log(`scale: ${scale}`);
     e.deltaY > 0 ? setScale(scale + ZOOM_SPEED) : setScale(scale - ZOOM_SPEED);
   };
 
   return (
-    <Box id="visualiser-container" margin="auto" width={window.screen.width}>
-      <svg transform={`scale(${scale})`} onWheel={onScroll} id="visualiser-canvas" />
+    <Box
+      onWheel={onScroll}
+      id="visualiser-container"
+      margin="auto"
+      height="100vh"
+      width={window.screen.width}
+    >
+      <svg
+        transform={`scale(${scale})`}
+        transform-origin="center"
+        onWheel={onScroll}
+        id="visualiser-canvas"
+        width="100%"
+      />
     </Box>
   );
 };
