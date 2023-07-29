a8a1f8fffe5b461734c43ff6483333218294127f
david zhou <davidzhou@davids-MBP.modem>
2023-07-29 22:38:58 +1000

    fix

--- client/src/utils/socket.ts
+++ client/src/utils/socket.ts
@@ -1,9 +1,5 @@
 import { io } from 'socket.io-client';
 
-<<<<<<< HEAD
-const URL = 'http://localhost:5001';
-=======
 const URL = 'http://localhost:8000';
->>>>>>> dev
 
 export const socket = io(URL);
