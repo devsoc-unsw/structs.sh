261c5d43d8177508cdcec84b5f7ca96961312057
david zhou <davidzhou@davids-MBP.modem>
2023-07-29 17:11:50 +1000

    refactor ui state

--- client/src/pages/src/framer-component/types/uiState.ts
+++ client/src/pages/src/framer-component/types/uiState.ts
@@ -1,15 +1,24 @@
+export enum VisualizerType {
+  LINKED_LIST = 'LINKED_LIST',
+  BINARY_TREE = 'BINARY_TREE',
+  GRAPH = 'GRAPH',
+  ARRAY = 'ARRAY',
+}
+
 export type UiState = {
   showHover: boolean;
   showClick: boolean;
   canDrag: boolean;
   debug: boolean;
   clickedEntity: string | null;
+  visualizerType: VisualizerType;
 }
 
 export const DEFAULT_UISTATE: UiState = {
   showHover: false,
   showClick: true,
-  canDrag: false,
-  debug: false,
-  clickedEntity: null
+  canDrag: true,
+  debug: true,
+  clickedEntity: null,
+  visualizerType: VisualizerType.LINKED_LIST,
 }
