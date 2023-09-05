f73a90af0cf1cbe54b0d659026b67fc5128b0e70
Shubh <Shu8h1234@gmail.com>
2023-09-05 17:44:04 +1000

    Modified sample c code

--- debugger/src/samples/malloc.c
+++ debugger/src/samples/malloc.c
@@ -1,1 +1,14 @@
-  struct node *node3 = malloc(sizeof(struct node));
+
+struct node {
+  int data;
+  struct node *next;
+};
+
+typedef struct list {
+  struct node *head;
+  int size;
+} List;
+
+int main(int argc, char **argv) {
+  node3->data = 3;
+}
