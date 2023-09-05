e0a4e6b16bafcda5e74f141e275d80592ccdb433
Shubh <Shu8h1234@gmail.com>
2023-09-05 14:23:59 +1000

    Got variable from ast malloc assignment for simple assignment, and pointer declaration AND initialisation with malloc

--- debugger/src/samples/linkedlist/ll_script.py
+++ debugger/src/samples/linkedlist/ll_script.py
@@ -1,23 +1,42 @@
-from pycparser import parse_file
+from pycparser import parse_file, c_ast
+
+# Define a visitor class to traverse the AST and find malloc assignments
+class MallocVisitor(c_ast.NodeVisitor):
+    def __init__(self):
+        self.malloc_variables = []
+
+    def visit_Assignment(self, node):
+        # Check if the assignment is of the form "variable = malloc(...)"
+        if isinstance(node.rvalue, c_ast.FuncCall) and isinstance(node.rvalue.name, c_ast.ID) and node.rvalue.name.name == 'malloc':
+            if isinstance(node.lvalue, c_ast.ID):
+                self.malloc_variables.append(node.lvalue.name)
+        self.generic_visit(node)
+
+    def visit_Decl(self, node):
+        # Check if the declaration initializes with malloc, e.g., "Type *var = malloc(...)"
+        if node.init and isinstance(node.init, c_ast.FuncCall) and isinstance(node.init.name, c_ast.ID) and node.init.name.name == 'malloc':
+            if isinstance(node.type, c_ast.PtrDecl):
+                var_name = node.name
+                self.malloc_variables.append(var_name)
+        self.generic_visit(node)
+
+    def visit_PtrDecl(self, node):
+        # Check for pointer declarations without initialization, e.g., "Type *var;"
+        if isinstance(node.type, c_ast.IdentifierType):
+            var_name = node.type.names[0]
+            self.malloc_variables.append(var_name)
+        self.generic_visit(node)
 
 # Input C code file and compiled output file
 input_c_file = "malloc2.c"
-compiled_output_file = "malloc2compiled"
 
-with open(input_c_file, "r") as c_file:
-    c_code = c_file.read()
+ast = parse_file(input_c_file, use_cpp=True)
 
-with open("temp.c", "w") as temp_file:
-    temp_file.write(c_code)
+# Create the visitor and traverse the AST
+malloc_visitor = MallocVisitor()
+malloc_visitor.visit(ast)
 
-import subprocess
-compile_command = f"gcc -E {temp_file.name} -o {compiled_output_file}"
-subprocess.run(compile_command, shell=True)
-
-ast = parse_file(compiled_output_file, use_cpp=False)
-
-print("Abstract Syntax Tree (AST):")
-print(ast)
-
-import os
-os.remove("temp.c")
+# Print the variable names assigned to malloc
+print("Variables assigned to malloc:")
+for var in malloc_visitor.malloc_variables:
+    print(var)
--- debugger/src/samples/linkedlist/malloc2.c
+++ debugger/src/samples/linkedlist/malloc2.c
@@ -1,6 +1,14 @@
-#include "malloc2.h"
+struct node {
+  int data;
+  struct node *next;
+};
+
+typedef struct list {
+  struct node *head;
+  int size;
+} List;
+
 
 int main(int argc, char **argv) {
-//    List *l = malloc(sizeof(*l));
-    l->head = malloc(sizeof(struct node));
+    struct node *node3 = malloc(sizeof(struct node));
 }
