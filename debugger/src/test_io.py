b79d787a21f49e67e27e321d4f9b094a915f2abf
James Treloar <TreloarJames@protonmail.ch>
2023-09-13 13:10:43 +1000

    Cleaned up issues with test_io

--- debugger/src/test_io.py
+++ debugger/src/test_io.py
@@ -1,30 +1,3 @@
-import pty
-import os
-import select
-from typing import Optional
-from time import sleep
-
-
-class IOManager:
-    max_read_bytes = 24 * 1024
-
-    def __init__(self):
-        (master, slave) = pty.openpty()
-        self.stdin = master
-        self.stdout = master
-        self.name = os.ttyname(slave)
-
-    def read(self) -> Optional[str]:
-        (data_to_read, _, _) = select.select([self.stdout], [], [], 1)
-        if data_to_read:
-            return os.read(self.stdout, self.max_read_bytes).decode()
-        else:
-            return None
-
-    def write(self, data: str):
-        os.write(self.stdin, data.encode())
-
-
 import gdb
 
 if __name__ == "__main__":
