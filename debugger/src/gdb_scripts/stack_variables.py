44b2714b13c60daf0fd0b36c3450bc1e0a8ca5f5
James Treloar <TreloarJames@protonmail.ch>
2023-09-14 19:49:01 +1000

    Frame info function

--- debugger/src/gdb_scripts/stack_variables.py
+++ debugger/src/gdb_scripts/stack_variables.py
@@ -1,6 +1,20 @@
 import gdb
 
 
+def get_frame_info():
+    gdb_frame_data: str = gdb.execute("bt", to_string=True)
+    gdb_frame_data = gdb_frame_data.split("\n", 1)[0]
+
+    frame_info: dict = {}
+    data1 = gdb_frame_data.split(") at", 1)
+
+    file_name = data1[1].strip().rstrip("0123456789")[:-1]
+    frame_info["file"] = file_name
+    frame_info["function"] = data1[0].replace("#0", "").strip().split(" ")[0]
+
+    return frame_info
+
+
 def get_stack_data():
     locals: str = gdb.execute("info locals", to_string=True)
     args: str = gdb.execute("info args", to_string=True)
@@ -26,7 +40,8 @@ def get_stack_data():
     return variables
 
 # Use this for testing
-if __name__ == "__main__":
-    print(get_stack_data())
+# if __name__ == "__main__":
+#     print(get_stack_data())
+#     print(get_frame_info())
 
 
