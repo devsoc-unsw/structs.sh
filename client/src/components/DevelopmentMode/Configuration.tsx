b90efff22bc3ea3249fc214491adbc944cc1ca68
jedwed <83894732+jedwed@users.noreply.github.com>
2023-09-22 14:57:35 +1000

    Check if type declarations are self referencing to detect potential linked list types

--- client/src/components/DevelopmentMode/Configuration.tsx
+++ client/src/components/DevelopmentMode/Configuration.tsx
@@ -1,3 +1,4 @@
+import classNames from 'classnames';
 import ConfigurationSelect from './ConfigurationSelect';
 import styles from 'styles/Configuration.module.css';
 
@@ -9,12 +10,34 @@ interface LinkedListAnnotations {
 }
 
 const Configuration = ({ typeDeclarations }) => {
+  const isSelfReferencing = (typeDeclaration) => {
+    console.log(typeDeclaration);
+    if (!('fields' in typeDeclaration)) {
+      return false;
+    }
+
+    return (
+      typeDeclaration.fields.length >= 2 &&
+      typeDeclaration.fields.some((field) => field.type.includes(typeDeclaration.name))
+    );
+  };
   return (
     <div>
-      {typeDeclarations.map((type, index) => (
-        <div key={index} className={styles.configuratorItem}>
-          <span>{type.name}</span>
-          <ConfigurationSelect />
+      {typeDeclarations.filter(isSelfReferencing).map((typeDeclaration, index: number) => (
+        <div key={index}>
+          <div className={styles.configuratorItem}>
+            <span>{typeDeclaration.name}</span>
+            <ConfigurationSelect />
+          </div>
+          {isSelfReferencing(typeDeclaration) &&
+            typeDeclaration.fields.map((field) => (
+              <div className={classNames(styles.configuratorItem, styles.field)}>
+                <span>
+                  {field.type} {field.name}
+                </span>
+                <ConfigurationSelect />
+              </div>
+            ))}
         </div>
       ))}
     </div>
