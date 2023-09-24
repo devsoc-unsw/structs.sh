cf6c07df206159230255367c9448827844f5fa25
jedwed <83894732+jedwed@users.noreply.github.com>
2023-09-24 14:39:31 +1000

    Replace code mirror with react-ace editor

--- client/src/components/DevelopmentMode/CodeEditor.tsx
+++ client/src/components/DevelopmentMode/CodeEditor.tsx
@@ -1,20 +1,21 @@
 import React, { FC } from 'react';
-import CodeMirror from '@uiw/react-codemirror';
-// import { javascript } from '@codemirror/lang-javascript';
-import { cpp } from '@codemirror/lang-cpp';
+import AceEditor from "react-ace";
 import { socket } from 'utils/socket';
 import Button from '@mui/material/Button';
 import UploadIcon from '@mui/icons-material/Upload';
 import 'styles/CodeEditor.css';
 
+import 'ace-builds/src-noconflict/mode-c_cpp';
+import 'ace-builds/src-noconflict/theme-github'
+
 const CodeEditor: FC = () => {
   const placeholder = '// Code your stuff below!';
   const [code, setCode] = React.useState(localStorage.getItem("code") || placeholder);
 
-  const onChange = React.useCallback((newCode: string) => {
+  const handleChange = (newCode: string) => {
     localStorage.setItem("code", newCode)
     setCode(newCode);
-  }, []);
+  }
 
   const sendCode = () => {
     socket.emit('mainDebug', code);
@@ -22,13 +23,14 @@ const CodeEditor: FC = () => {
 
   return (
     <>
-      <CodeMirror
-        value={code}
-        height="100%"
-        extensions={[cpp()]}
-        onChange={onChange}
-        theme="light"
-      />
+      <AceEditor value={code} onChange={handleChange} height="100%" width="100%" mode="c_cpp" theme="github"/>
+      {/* <CodeMirror */}
+      {/*   value={code} */}
+      {/*   height="100%" */}
+      {/*   extensions={[cpp()]} */}
+      {/*   onChange={onChange} */}
+      {/*   theme="light" */}
+      {/* /> */}
       <Button onClick={sendCode} variant="contained" endIcon={<UploadIcon />}>
         Run
       </Button>
