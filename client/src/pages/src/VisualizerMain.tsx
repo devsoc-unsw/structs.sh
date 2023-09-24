e12af3fc91bfb02c818f753ef75bf60ed546eda0
jedwed <83894732+jedwed@users.noreply.github.com>
2023-09-25 02:01:39 +1000

    Visualiser canvas fills entire pane

--- client/src/pages/src/VisualizerMain.tsx
+++ client/src/pages/src/VisualizerMain.tsx
@@ -3,7 +3,6 @@ import { DEFAULT_UISTATE, UiState } from './visualizer-component/types/uiState';
 import { parserFactory } from './visualizer-component/parser/parserFactory';
 import { visualizerFactory } from './visualizer-component/visulizer/visualizerFactory';
 import { BackendState } from './visualizer-component/types/backendType';
-import { Timeline } from './visualizer-component/util/timeline';
 import { useFrontendStateStore } from './visualizer-component/visaulizerStateStore';
 import { LocalAnnotationBase, UserAnnotation } from './visualizer-component/types/annotationType';
 import { useUiStateStore } from './visualizer-component/uiStateStore';
@@ -15,11 +14,7 @@ export interface RoutesProps {
 }
 
 // Future support different parser
-const VisualizerMain: React.FC<RoutesProps> = ({
-  backendState,
-  getDummyNextState,
-  getNextState,
-}: RoutesProps) => {
+const VisualizerMain: React.FC<RoutesProps> = ({ backendState }: RoutesProps) => {
   const [settings, setSettings] = useState<UiState>(DEFAULT_UISTATE);
   const VisComponent = visualizerFactory(settings);
   const [parser] = useState(parserFactory(settings));
@@ -58,37 +53,35 @@ const VisualizerMain: React.FC<RoutesProps> = ({
   }, [backendState]);
 
   const visualizerRef = useRef(null);
-  const [dimensions, setDimensions] = useState({ width: 0, height: 0 });
+  const [dimensions, setDimensions] = useState({ width: 1000, height: 1000 });
 
-  useEffect(() => {
-    const observer = new ResizeObserver((entries) => {
-      if (!visualizerRef) return;
-
-      entries.forEach((element) => {
-        if (element.target instanceof HTMLElement && element.target.className === 'visualizer') {
-          const { width, height } = element.contentRect;
-
-          if (dimensions.height === 0) {
-            setDimensions({ width, height });
-            dimensions.height = height;
-          }
-        }
-      });
-    });
-
-    if (visualizerRef.current) {
-      observer.observe(visualizerRef.current);
-    }
-    return () => {
-      if (visualizerRef.current) {
-        observer.unobserve(visualizerRef.current);
-      }
-    };
-  }, [visualizerRef]);
+  // useEffect(() => {
+  //   const observer = new ResizeObserver((entries) => {
+  //     if (!visualizerRef) return;
+  //
+  //     entries.forEach((element) => {
+  //       if (element.target instanceof HTMLElement && element.target.className === 'visualizer') {
+  //         const { width, height } = element.contentRect;
+  //
+  //         if (dimensions.height === 0) {
+  //           setDimensions({ width, height });
+  //           dimensions.height = height;
+  //         }
+  //       }
+  //     });
+  //   });
+  //
+  //   if (visualizerRef.current) {
+  //     observer.observe(visualizerRef.current);
+  //   }
+  //   return () => {
+  //     if (visualizerRef.current) {
+  //       observer.unobserve(visualizerRef.current);
+  //     }
+  //   };
+  // }, [visualizerRef]);
 
   return (
-    <div className="container">
-      <div className="linked-list">
         <div className="visualizer" ref={visualizerRef} style={{ overflow: 'hidden' }}>
           <VisComponent
             settings={settings}
@@ -97,22 +90,6 @@ const VisualizerMain: React.FC<RoutesProps> = ({
             dimensions={dimensions}
           />
         </div>
-        <div className="timeline">
-          <Timeline
-            nextStateDummy={getDummyNextState}
-            nextState={getNextState}
-            forwardState={() => {
-              useFrontendStateStore.getState().forwardState();
-              console.log('Forward triggered', useFrontendStateStore.getState().currStateIdx);
-            }}
-            backwardState={() => {
-              useFrontendStateStore.getState().backwardState();
-              console.log('Forward triggered', useFrontendStateStore.getState().currStateIdx);
-            }}
-          />
-        </div>
-      </div>
-    </div>
   );
 };
 
