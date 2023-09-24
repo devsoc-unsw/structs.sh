7dad7729865023a0e6424652cb614e1e4e14656e
jedwed <83894732+jedwed@users.noreply.github.com>
2023-09-25 00:12:41 +1000

    Added linked list program from COMP1511 Programming Fundamentals as a placeholder for the editor

--- client/src/components/DevelopmentMode/CodeEditor.tsx
+++ client/src/components/DevelopmentMode/CodeEditor.tsx
@@ -7,8 +7,42 @@ import 'styles/CodeEditor.css';
 
 import 'ace-builds/src-noconflict/mode-c_cpp';
 
+const placeholder = `// Linked List Program from COMP1511 Lecture 7
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+  struct node *next;
+  int data;
+};
+
+int main(void) {
+  // 3 separate allocations on the heap
+  struct node *head = malloc(sizeof(struct node));
+  struct node *linked_list_2 = malloc(sizeof(struct node));
+  struct node *linked_list_3 = malloc(sizeof(struct node));
+
+  // initialise the nodes
+  head->data = 1;
+  linked_list_2->data = 2;
+  linked_list_3->data = 3;
+
+  // Set the head next pointer
+  // to point to the location of linked_list_2
+  head->next = linked_list_2;
+  linked_list_2->next = linked_list_3;
+  linked_list_3->next = NULL;
+
+  struct node *curr = head;
+
+  while (curr != NULL) {
+    printf("%d\n", curr->data);
+    curr = curr->next;
+  }
+}
+`;
+
 const CodeEditor = ({ currLine }: { currLine: number }) => {
-  const placeholder = '// Code your stuff below!';
   const [code, setCode] = React.useState(localStorage.getItem('code') || placeholder);
 
   const markers: IMarker[] = [
@@ -25,7 +59,6 @@ const CodeEditor = ({ currLine }: { currLine: number }) => {
   const handleChange = (newCode: string) => {
     localStorage.setItem('code', newCode);
     setCode(newCode);
-    console.log(currLine);
   };
 
   const sendCode = () => {
