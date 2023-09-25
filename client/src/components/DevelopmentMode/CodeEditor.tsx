e6380001fb5872574afebcf7536579fd8fd0ef47
jedwed <83894732+jedwed@users.noreply.github.com>
2023-09-25 10:03:28 +1000

    Add autocompletion and snippets to code editor, change highlight color to be lighter

--- client/src/components/DevelopmentMode/CodeEditor.tsx
+++ client/src/components/DevelopmentMode/CodeEditor.tsx
@@ -2,6 +2,8 @@ import AceEditor, { IMarker } from 'react-ace';
 import 'styles/CodeEditor.css';
 
 import 'ace-builds/src-noconflict/mode-c_cpp';
+import 'ace-builds/src-noconflict/ext-language_tools';
+import 'ace-builds/src-noconflict/theme-tomorrow';
 
 const CodeEditor = ({
   code,
@@ -27,10 +29,14 @@ const CodeEditor = ({
     <AceEditor
       value={code}
       onChange={handleSetCode}
+      mode="c_cpp"
+      theme="tomorrow"
       height="100%"
       width="100%"
-      mode="c_cpp"
       markers={markers}
+      enableSnippets={true}
+      enableLiveAutocompletion={true}
+      enableBasicAutocompletion={true}
     />
   );
 };
