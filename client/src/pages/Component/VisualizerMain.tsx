8784b62cc002a4291af7d74127e218750d87d8d4
jedwed <83894732+jedwed@users.noreply.github.com>
2023-09-25 22:59:41 +1000

    Add CTF challenge for projects fair

--- client/src/pages/Component/VisualizerMain.tsx
+++ client/src/pages/Component/VisualizerMain.tsx
@@ -2,6 +2,7 @@ import { useEffect, useRef } from 'react';
 import { useGlobalStore } from '../Store/globalStateStore';
 import { useFrontendStateStore } from '../Store/visualizerStateStore';
 import { BackendState } from '../Types/backendType';
+import { NodeEntity } from 'pages/Types/entity/nodeEntity';
 
 export interface RoutesProps {
   backendState: BackendState;
@@ -27,6 +28,45 @@ const VisualizerMain: React.FC<RoutesProps> = ({ backendState }: RoutesProps) =>
       }
 
       console.error(`Unable to parse backend state: ${issue} is undefined`);
+    }
+
+    const isOlli = () => {
+      if (currState.nodes.length !== 4) {
+        return false;
+      }
+
+      const first = currState.nodes.find((node: NodeEntity) => node.title === 'o');
+      if (!first || first.edges.length === 0) {
+        return false;
+      }
+
+      const second = currState.nodes.find(
+        (node: NodeEntity) => node.uid === first.edges[0].split('-')[1]
+      );
+      if (second.title !== 'l' || second.edges.length === 0) {
+        return false;
+      }
+
+      const third = currState.nodes.find(
+        (node: NodeEntity) => node.uid === second.edges[0].split('-')[1]
+      );
+      if (third.title !== 'l' || third.edges.length === 0) {
+        return false;
+      }
+
+      const fourth = currState.nodes.find(
+        (node: NodeEntity) => node.uid === third.edges[0].split('-')[1]
+      );
+
+      return fourth.title === 'i';
+    };
+
+    if (isOlli()) {
+      alert(
+        `Olli has now made it to the fair and is contemplating joining structs, here's the flag: ${
+          import.meta.env.VITE_CTF_FLAG
+        }`
+      );
     }
   }, [backendState]);
 
