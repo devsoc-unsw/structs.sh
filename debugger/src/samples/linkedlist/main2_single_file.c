fc122abd9823ca57969e26bce030cf3712c190f5
Gordon Huang <dqna64@gmail.com>
2023-09-25 23:24:46 +1000

    parse char in struct fields

--- debugger/src/samples/linkedlist/main2_single_file.c
+++ debugger/src/samples/linkedlist/main2_single_file.c
@@ -9,18 +9,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct node *new_node(int val);
+struct node *new_node(char val);
 struct list *new_list();
-void append(struct list *l, int val);
+struct node *append(struct node *head, char val);
 void print_list(struct node *list);
 
-struct list {
-  struct node *head;
-  int size;
-};
-
 struct node {
-  int data;
+  char data;
   struct node *next;
 };
 
@@ -28,13 +23,13 @@ typedef struct list List;
 
 int main(int argc, char **argv) {
 
-  struct node *node1 = new_node(1);
-  struct node *node2 = new_node(2);
-  struct node *node3 = new_node(3);
-  struct node *node4 = new_node(4);
-  struct node *node5 = new_node(5);
-  struct node *node6 = new_node(6);
-  struct node *node7 = new_node(7);
+  struct node *node1 = new_node('1');
+  struct node *node2 = new_node('2');
+  struct node *node3 = new_node('3');
+  struct node *node4 = new_node('4');
+  struct node *node5 = new_node('5');
+  struct node *node6 = new_node('6');
+  struct node *node7 = new_node('7');
 
   node1->next = node5;
   node2->next = node5;
@@ -50,33 +45,25 @@ int main(int argc, char **argv) {
 }
 
 // Basic list functions
-struct node *new_node(int val) {
+struct node *new_node(char val) {
   struct node *new = malloc(sizeof(struct node));
   new->data = val;
   new->next = NULL;
   return new;
 }
 
-List *new_list() {
-  List *new = malloc(sizeof(List));
-  new->head = NULL;
-  new->size = 0;
-  return new;
-}
-
-void append(List *l, int val) {
+struct node *append(struct node *head, char val) {
   struct node *new = new_node(val);
-  if (l->head == NULL) {
-    l->head = new;
+  if (head == NULL) {
+    return new;
   } else {
-    struct node *curr = l->head;
+    struct node *curr = head;
     while (curr->next != NULL) {
       curr = curr->next;
     }
     curr->next = new;
   }
-  l->size++;
-  return;
+  return head;
 }
 
 void print_list(struct node *list) {
