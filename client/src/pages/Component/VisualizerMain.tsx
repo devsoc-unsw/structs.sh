5fa58e37eecf591563da8796255e9a6cf9986e3b
jedwed <83894732+jedwed@users.noreply.github.com>
2023-09-25 23:07:03 +1000

    Changed linked list word from olli to dev after the groundbreaking realisation that ollie is actually spelt with an 'e' at the end

--- client/src/pages/Component/VisualizerMain.tsx
+++ client/src/pages/Component/VisualizerMain.tsx
@@ -1,8 +1,8 @@
 import { useEffect, useRef } from 'react';
+import { NodeEntity } from 'pages/Types/entity/nodeEntity';
 import { useGlobalStore } from '../Store/globalStateStore';
 import { useFrontendStateStore } from '../Store/visualizerStateStore';
 import { BackendState } from '../Types/backendType';
-import { NodeEntity } from 'pages/Types/entity/nodeEntity';
 
 export interface RoutesProps {
   backendState: BackendState;
@@ -30,12 +30,12 @@ const VisualizerMain: React.FC<RoutesProps> = ({ backendState }: RoutesProps) =>
       console.error(`Unable to parse backend state: ${issue} is undefined`);
     }
 
-    const isOlli = () => {
-      if (currState.nodes.length !== 4) {
+    const isDev = () => {
+      if (currState.nodes.length !== 3) {
         return false;
       }
 
-      const first = currState.nodes.find((node: NodeEntity) => node.title === 'o');
+      const first = currState.nodes.find((node: NodeEntity) => node.title === 'd');
       if (!first || first.edges.length === 0) {
         return false;
       }
@@ -43,25 +43,18 @@ const VisualizerMain: React.FC<RoutesProps> = ({ backendState }: RoutesProps) =>
       const second = currState.nodes.find(
         (node: NodeEntity) => node.uid === first.edges[0].split('-')[1]
       );
-      if (second.title !== 'l' || second.edges.length === 0) {
+      if (second.title !== 'e' || second.edges.length === 0) {
         return false;
       }
 
       const third = currState.nodes.find(
         (node: NodeEntity) => node.uid === second.edges[0].split('-')[1]
       );
-      if (third.title !== 'l' || third.edges.length === 0) {
-        return false;
-      }
 
-      const fourth = currState.nodes.find(
-        (node: NodeEntity) => node.uid === third.edges[0].split('-')[1]
-      );
-
-      return fourth.title === 'i';
+      return third.title === 'v';
     };
 
-    if (isOlli()) {
+    if (isDev()) {
       alert(
         `Olli has now made it to the fair and is contemplating joining structs, here's the flag: ${
           import.meta.env.VITE_CTF_FLAG
