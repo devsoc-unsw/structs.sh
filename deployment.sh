54949550d8fa60b20b6e3a523cefcb594ee9bc65
david zhou <davidzhou@davids-MacBook-Pro-3.local>
2024-06-28 12:41:05 +1000

    save

--- deployment.sh
+++ deployment.sh
@@ -1,3 +1,12 @@
+sudo curl -L "https://github.com/docker/compose/releases/download/v2.17.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+
+sudo apt-get update
+sudo apt-get install docker.io -y
+sudo systemctl start docker
+
+
 rsync -avz --exclude 'debugger/src/__pycache__' --exclude 'client/node_modules' --exclude '.git'  --exclude '.env' \
--e "ssh -i ~/.ssh/structs-deploy.pem" \
-. ubuntu@ec2-3-25-92-212.ap-southeast-2.compute.amazonaws.com:~/app
+-e "ssh -i ~/.ssh/struct-deploy.pem" \
+. ubuntu@ec2-54-80-88-177.compute-1.amazonaws.com:~/app
