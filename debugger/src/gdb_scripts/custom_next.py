4dffe410c5237181bda933a577b7d755b7b2b200
Shubh Shah <Shu8h1234@gmail.com>
2024-08-27 13:19:23 +1000

    Commented out 'start': true and fixed error that occurs before exiting

--- debugger/src/gdb_scripts/custom_next.py
+++ debugger/src/gdb_scripts/custom_next.py
@@ -99,10 +99,12 @@ class CustomNextCommand(gdb.Command):
         self.debug_session = debug_session
         self.heap_data = {}
         self.break_on_all_user_defined_functions()
-        start_data = {
-            "started": True
-        }
-        send_backend_data_to_server(self.user_socket_id, backend_data=start_data)
+        
+        # Send start_data across socket
+        # start_data = {
+        #     "started": True
+        # }
+        # send_backend_data_to_server(self.user_socket_id, backend_data=start_data)
 
     def invoke(self, arg=None, from_tty=None):
         # TODO: detect end of debug session
@@ -113,23 +115,6 @@ class CustomNextCommand(gdb.Command):
         #     return
 
         print("\n=== Running CustomNextCommand in gdb...")
-
-        # Check if the program has exited by evaluating $_exitcode
-        try:
-            exit_code_str = gdb.execute("print $_exitcode", to_string=True)
-            exit_code = exit_code_str.split('=')[1].strip()
-            if exit_code != 'void':
-                print("Program has exited with code:", exit_code)
-                exit_data = {
-                    "exited": True
-                }
-                send_backend_data_to_server(self.user_socket_id, backend_data=exit_data)
-                return
-
-        except gdb.error:
-            print("Error determining exit code, assuming program has not exited.")
-
-
 
         variable_freed = False
 
@@ -325,6 +310,20 @@ class CustomNextCommand(gdb.Command):
 
         if not variable_freed:
             gdb.execute('next')
+
+        # Immediately after executing next, check if the program has exited by evaluating $_exitcode
+        try:
+            exit_code_str = gdb.execute("print $_exitcode", to_string=True)
+            exit_code = exit_code_str.split('=')[1].strip()
+            if exit_code != 'void':
+                print("Program has exited with code:", exit_code)
+                exit_data = {
+                    "exited": True
+                }
+                send_backend_data_to_server(self.user_socket_id, backend_data=exit_data)
+                return
+        except gdb.error:
+            print("Error determining exit code, assuming program has not exited.")
             
         # TODO: Immediately after executing next, we need to check whether the program
         # is waiting for stdin. If it is, then we need to send a signal to the
