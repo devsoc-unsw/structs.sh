2a1d3191eaef16a06da0bed2f4e60acfb645ee56
atri <46995931+Chinosu@users.noreply.github.com>
2025-06-28 22:22:09 +1000

    remove silly debug asserts

--- debugger/src/debugger/base_debugger.py
+++ debugger/src/debugger/base_debugger.py
@@ -38,11 +38,6 @@ class BaseDebugger:
         self._did_init = False
 
     async def init(self, executable_path: str) -> None:
-        import os, pwd, logging
-
-        assert not os.geteuid()
-        logging.info(f"??> {os.geteuid()}, {pwd.getpwuid(os.geteuid()).pw_name}")
-
         self.fd_master, self.fd_slave = os.openpty()
         _disable_echo(self.fd_slave)
         self.process = await create_subprocess_exec(
