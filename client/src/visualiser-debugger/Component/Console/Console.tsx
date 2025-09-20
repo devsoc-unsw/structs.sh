486e29249a569252007a46b963752363cbc25c0c
Angelin <z5438386@student.unsw.edu.au>
2025-09-21 01:42:09 +1000

    Finished some command implementation

--- client/src/visualiser-debugger/Component/Console/Console.tsx
+++ client/src/visualiser-debugger/Component/Console/Console.tsx
@@ -6,7 +6,7 @@ import { useGlobalStore } from 'visualiser-debugger/Store/globalStateStore';
 import { useFrontendStateStore } from 'visualiser-debugger/Store/frontendStateStore';
 import { useUserFsStateStore } from 'visualiser-debugger/Store/userFsStateStore';
 import CustomCaret from './CustomCaret';
-import { IFileFileNode, IFileDirNode } from '../FileTree/FS/IFileSystem';
+import { IFileFileNode } from '../FileTree/FS/IFileSystem';
 import useConsolePathStore from 'visualiser-debugger/Store/consolePathStore';
 
 type ConsoleProp = {
@@ -14,18 +14,18 @@ type ConsoleProp = {
 };
 
 const Console = ({ scrollToBottom }: ConsoleProp) => {
-  const { currWorkingDir, setCurrWorkingDir } = useConsolePathStore();
-  const [PREFIX, setPrefix] = useState(`structs.sh/${currWorkingDir} % `);
-  const [input, setInput] = useState(PREFIX);
+  const { prefix, setPrefix, setCurrWorkingDir, clearConsole, printWorkingDir, 
+    createNewDir, changeDir, listFiles, createNewFile, removeFile } = useConsolePathStore();
+  // /const [PREFIX, setPrefix] = useState(`structs.sh/${currWorkingDir} % `);
+  const [input, setInput] = useState(prefix);
   const inputElement = useRef<HTMLInputElement>(null);
 
   const consoleChunks = useGlobalStore((state) => state.consoleChunks);
   const isCompiled = useFrontendStateStore((state) => state.isActive);
   const appendConsoleChunks = useGlobalStore((state) => state.appendConsoleChunks);
-  const { resetConsoleChunks } = useGlobalStore();
   const { fileSystem, currFocusFilePath } = useUserFsStateStore();
 
-  const updatePrefixPath = (newPath : string) => {
+  const updatePrefixPath = (newPath : string) => { // Ini pindahin ke consolePathStore
     setCurrWorkingDir(newPath);
     setPrefix(`structs.sh/${newPath} % `);
   }
@@ -33,124 +33,38 @@ const Console = ({ scrollToBottom }: ConsoleProp) => {
   useEffect(() => {
     if (isCompiled) {
       const file = fileSystem.getFileFromPath(currFocusFilePath) as IFileFileNode;
-      appendConsoleChunks(`${PREFIX}gcc -g ${file.name} -o a\n`);
+      appendConsoleChunks(`${prefix}gcc -g ${file.name} -o a\n`);
       setInput('');
     } else {
-      setInput(PREFIX);
+      setInput(prefix);
     }
-  }, [isCompiled, PREFIX]);
+  }, [isCompiled, prefix]);
 
   // Every time when user add input to console, check the corresponding command
   useEffect(() => {
-    const lastElem = consoleChunks.length - 1;
-    if (consoleChunks[lastElem] === `${PREFIX}clear\n`) {
-      resetConsoleChunks();
-    } else if (consoleChunks[lastElem] === `${PREFIX}pwd\n`) {
-      const newChunks = currWorkingDir + '\n';
-      appendConsoleChunks('/' + newChunks);
-    } else if (consoleChunks.length > 0 && consoleChunks[lastElem].startsWith(`${PREFIX}mkdir`)) {
-      const str = consoleChunks[lastElem].trim();
-      const newDirName = str.replace(`${PREFIX}mkdir `, '');
+    if (consoleChunks.length <= 0) return;
+    const consoleIndex = consoleChunks.length - 1;
+    if (!consoleChunks[consoleIndex].startsWith(prefix)) return;
 
-      // Error checking
-      if (newDirName.length === 0) {
-        appendConsoleChunks('mkdir: missing operand\n');
-        return;
-      } else if (newDirName.includes('/')) {
-        appendConsoleChunks(`mkdir: cannot create directory ${newDirName}`)
-      }
-
-      // First thing -> findNodeByPath
-      const newFolder: IFileDirNode = {
-        name: newDirName,
-        path: `${currWorkingDir}/${newDirName}`,
-        type: 'dir',
-        children: {},
-        parentPath: currWorkingDir
-      }
-
-      if (!fileSystem.addDir(newFolder)) {
-        alert('failed to create a new directory'); // This can be changed to a modal later on instead of alert
-        return;
-      }
-
-      fileSystem.saveChanges();
-    } else if (consoleChunks.length > 0 && consoleChunks[lastElem].startsWith(`${PREFIX}cd`)) {
-      // dirPath is the directory name
-      const dirPath = consoleChunks[lastElem].trim().replace(`${PREFIX}cd `, '');
-
-      // Cannot go further than the root directory
-      if (dirPath === '..') {
-        if (currWorkingDir === 'root') return;
-        
-        // Destructure the current working directory
-        const dirPathArray = currWorkingDir.split('/');
-        dirPathArray.pop();
-        const newPath = dirPathArray.join('/');
-        updatePrefixPath(newPath);
-        return;
-      }
-
-      // Update directory path to the root right away
-      if (dirPath === 'root') {
-        setCurrWorkingDir('root');
-        updatePrefixPath('root');
-        return;
-      }
-
-      const newPath = `${currWorkingDir}/${dirPath}`;
-      const getDir = fileSystem.getDirFromPath(newPath);
-      if (getDir && (getDir.type != 'dir' || getDir.name == '/root')) {
-        appendConsoleChunks(`cd: ${dirPath}: Not a directory\n`);
-        return;
-      }
-
-      const doesDirExists = fileSystem.doesDirExists(newPath);
-      if (!doesDirExists) {
-        appendConsoleChunks(`cd: ${dirPath}: No such file or directory\n`);
-        return;
-      }
-
-      updatePrefixPath(newPath);
-    } else if (consoleChunks.length > 0 && consoleChunks[lastElem].startsWith(`${PREFIX}ls`)) {
-      const files = fileSystem.getDirFromPath(currWorkingDir)?.children;
-      let list = '';
-      for (const index in files) {
-        const nodeName = files[index].name;
-        if (nodeName.trim().length === 0) continue;
-        list = `${list} ${nodeName.trim()}`
-      }
-      list = list.trim();
-      appendConsoleChunks(`${list}\n`);
-    } else if (consoleChunks.length > 0 && consoleChunks[lastElem].startsWith(`${PREFIX}touch`)) {
-      const fileName = consoleChunks[lastElem].trim().replace(`${PREFIX}touch `, '');
-      if (fileName.includes('/')) {
-        appendConsoleChunks(`Unable to create file: '\' cannot exists in a file name.\n`);
-        return ;
-      }
-
-      // Handle creating multiple files at the same time
-      const files = fileName.split(/\s+/);
-      for (const file of files) {
-        const newFile: IFileFileNode = {
-          name: file,
-          path: `${currWorkingDir}/${file}`,
-          type: 'file',
-          data: '',
-          parentPath: currWorkingDir,
-        }
-        fileSystem.addFile(newFile);
-      }
-    } else if (consoleChunks.length > 0 && consoleChunks[lastElem].startsWith(`${PREFIX}rm`)) {
-      const fileName = consoleChunks[lastElem].trim().replace(`${PREFIX}rm `, '');
-      const filePath = `${currWorkingDir}/${fileName}`;
-      // Check if file exists
-      const fileToBeDeleted = fileSystem.getFileFromPath(filePath);
-      if (!fileToBeDeleted) {
-        appendConsoleChunks(`rm: ${fileToBeDeleted}: No such file exists`);
-        return;
-      }
-      fileSystem.deleteFile(fileToBeDeleted);
+    // Running user command
+    const command = consoleChunks[consoleIndex].trim().replace(`${prefix}`, '');
+    if (command === 'clear') clearConsole();
+    else if (command === 'pwd') printWorkingDir();
+    else if (command.startsWith('ls')) listFiles();
+    else if (command.startsWith('mkdir')) {
+      const dirName = command.replace('mkdir ', '');
+      createNewDir(dirName);
+    }
+    else if (command.startsWith('cd')) {
+      const dirPath = command.replace ('cd ', '');
+      changeDir(dirPath);
+    } 
+    else if (command.startsWith('touch')) {
+      const fileName = command.replace('touch ', '');
+      createNewFile(fileName);
+    } else if (command.startsWith('rm')) {
+      const fileName = command.replace('rm ', '');
+      removeFile(fileName);
     }
   }, [consoleChunks])
 
@@ -161,7 +75,7 @@ const Console = ({ scrollToBottom }: ConsoleProp) => {
     }
 
     // Ensure structs.sh prefix can't be deleted
-    if (currInput.startsWith(PREFIX)) {
+    if (currInput.startsWith(prefix)) {
       setInput(currInput)
     }
   };
@@ -172,7 +86,7 @@ const Console = ({ scrollToBottom }: ConsoleProp) => {
       return;
     }
 
-    setInput(PREFIX);
+    setInput(prefix);
   };
 
   const focus = () => {
