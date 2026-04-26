d0e3e961a91e07a4e43061be18edb783c9b70f3b
Danar Prakosa <danarprakosa@gmail.com>
2026-04-26 22:51:36 +1000

    chore: migrate to pigment css

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import '@mui/material-pigment-css/styles.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,19 +2,25 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import checker from 'vite-plugin-checker';
+import { pigment } from '@pigment-css/vite-plugin';
+import { structsTheme } from './src/structsThemes';
+
+/**
+ * @type {import('@pigment-css/vite-plugin').PigmentOptions}
+ */
+const pigmentConfig = {
+  transformLibraries: ['@mui/material'],
+  theme: structsTheme
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
   optimizeDeps: {
-    include: ['@emotion/react', '@emotion/styled', '@mui/material/Tooltip'],
+    include: ['@mui/material/Tooltip'],
   },
   plugins: [
-    react({
-      jsxImportSource: '@emotion/react',
-      babel: {
-        plugins: ['@emotion/babel-plugin'],
-      },
-    }),
+    pigment(pigmentConfig),
+    react(),
     tsconfigPaths(),
     checker({ typescript: true }),
   ],
