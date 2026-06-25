03866b54357e18e45eedf9c1639fb04cfa595f37
Aaron Yang <z5766989@unsw.edu.au>
2026-06-26 01:36:15 +1000

    Refactored constants in FloatingWindow.tsx

--- client/src/constants/ui.ts
+++ client/src/constants/ui.ts
@@ -6,3 +6,10 @@ export const DEBUG = false
 
 // Controls.tsx
 export const SPEED_OPTIONS = [0.25, 0.5, 0.75, 1.0, 1.25, 1.5, 1.75, 2];
+
+// FloatingWindow.tsx
+export const FLOATING_WINDOW_BOTTOM = '54px';
+export const FLOATING_WINDOW_MAX_HEIGHT = '80vh';
+export const FLOATING_WINDOW_PADDING = '10px';
+export const FLOATING_WINDOW_MIN_WIDTH = '30vw';
+export const FLOATING_WINDOW_MAX_WIDTH = '50vw';
