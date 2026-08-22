0fcde030edeb530d5f8c7ef70a2f1388b1189f17
Andy <andy.ruan2005@gmail.com>
2026-08-22 11:03:30 +1000

    add an enum entry for the graph data structure

--- client/src/visualiser-src/common/typedefs.ts
+++ client/src/visualiser-src/common/typedefs.ts
@@ -17,6 +17,7 @@ export enum DataStructure {
   BINARY_SEARCH_TREE = 'Binary Search Trees',
   AVL_TREE = 'AVL Trees',
   SORTING = 'Sorting Algorithms',
+  GRAPH = 'Graphs',
 }
 
 export interface Documentation {
