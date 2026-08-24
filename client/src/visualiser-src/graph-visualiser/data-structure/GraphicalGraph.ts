8a866805b3eac09a535b9296999a20250b57bb1e
Andy <andy.ruan2005@gmail.com>
2026-08-24 12:22:25 +1000

    add LHS panel documentation for graph

--- client/src/visualiser-src/graph-visualiser/data-structure/GraphicalGraph.ts
+++ client/src/visualiser-src/graph-visualiser/data-structure/GraphicalGraph.ts
@@ -5,7 +5,33 @@ import { injectIds } from '@/visualiser-src/common/helpers';
 import { Documentation } from '@/visualiser-src/common/typedefs';
 
 export default class GraphicalGraph extends GraphicalDataStructure {
-  private static documentation: Documentation = injectIds({});
+  // this stuff is for the LHS panel of the graphs page
+  private static documentation: Documentation = injectIds({
+    'add vertex': {
+      args: ['value', 'index'],
+      description: 'Create a value at the given index.',
+    },
+    'remove vertex': {
+      args: ['value', 'index'],
+      description: 'a value at the given index.',
+    },
+    'add edge': {
+      args: ['index1', 'index2'],
+      description: 'Add an edge between two vertices',
+    },
+    'remove edge': {
+      args: ['index1', 'index2'],
+      description: 'Remove an edge between two vertices.',
+    },
+    search: {
+      args: ['value'],
+      description: 'Search for a value in the linked list.',
+    },
+    delete: {
+      args: ['index'],
+      description: 'Delete a node by the index given.',
+    },
+  });
 
   public get documentation(): Documentation {
     return GraphicalGraph.documentation;
